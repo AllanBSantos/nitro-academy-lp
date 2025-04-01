8c0d8cf3d0ed5178e5460d0d68e25f2f4a5e9a1e
AllanBSantos <allanbarrossantos@hotmail.com>
2025-04-01 17:21:07 -0300

    fix: spaces on strapi projects

--- src/app/components/InfoCard.tsx
+++ src/app/components/InfoCard.tsx
@@ -82,7 +82,13 @@ const InfoCard: FC<InfoCardProps> = ({
         {classModel && <p>{classModel}</p>}
         {target && <p>{target}</p>}
         {requirements && <p>{requirements}</p>}
-        {projects && <p>{projects}</p>}
+        {projects && (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: projects.replace(/\n/g, "<br />"),
+            }}
+          />
+        )}
         {homework && <p>{homework}</p>}
         {highlight && <p>{highlight}</p>}
         {additionalInfo && <p>{additionalInfo}</p>}
