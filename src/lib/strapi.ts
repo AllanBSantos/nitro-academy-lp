d7e49857ea56c6fad1d5571416557aad1c34660d
AllanBSantos <allanbarrossantos@hotmail.com>
2025-04-11 15:05:04 -0300

    removed token for courses

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -2,18 +2,12 @@ import { Course, Mentor, Review } from "@/types/strapi";
 
 const STRAPI_API_URL =
   process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337";
-const STRAPI_TOKEN = process.env.STRAPI_TOKEN;
 export async function fetchCourses(
   locale: string = "pt-BR"
 ): Promise<Course[]> {
   try {
     const response = await fetch(
-      `${STRAPI_API_URL}/api/cursos?fields[0]=id&fields[1]=titulo&fields[2]=descricao&fields[3]=nota&fields[4]=nivel&fields[5]=modelo&fields[6]=objetivo&fields[7]=pre_requisitos&fields[8]=projetos&fields[9]=tarefa_de_casa&fields[10]=preco&fields[11]=parcelas&fields[12]=destaques&fields[13]=slug&fields[14]=link_pagamento&populate[imagem][fields][0]=url&populate[mentor][populate][imagem][fields][0]=url&populate[videos][populate]=video&populate[tags][fields][0]=nome&populate[cronograma][fields][0]=data_fim&populate[cronograma][fields][1]=data_inicio&populate[cronograma][fields][2]=dia&populate[cronograma][fields][3]=horario&locale=${locale}&_=${Date.now()}`,
-      {
-        headers: {
-          Authorization: `Bearer ${STRAPI_TOKEN}`,
-        },
-      }
+      `${STRAPI_API_URL}/api/cursos?fields[0]=id&fields[1]=titulo&fields[2]=descricao&fields[3]=nota&fields[4]=nivel&fields[5]=modelo&fields[6]=objetivo&fields[7]=pre_requisitos&fields[8]=projetos&fields[9]=tarefa_de_casa&fields[10]=preco&fields[11]=parcelas&fields[12]=destaques&fields[13]=slug&fields[14]=link_pagamento&populate[imagem][fields][0]=url&populate[mentor][populate][imagem][fields][0]=url&populate[videos][populate]=video&populate[tags][fields][0]=nome&populate[cronograma][fields][0]=data_fim&populate[cronograma][fields][1]=data_inicio&populate[cronograma][fields][2]=dia&populate[cronograma][fields][3]=horario&locale=${locale}&_=${Date.now()}`
     );
 
     if (!response.ok) {
@@ -33,13 +27,7 @@ export async function fetchCourse(
   locale: string = "pt-BR"
 ): Promise<Course> {
   const response = await fetch(
-    `${STRAPI_API_URL}/api/cursos/${id}?populate=*&locale=${locale}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${STRAPI_TOKEN}`,
-      },
-    }
+    `${STRAPI_API_URL}/api/cursos/${id}?populate=*&locale=${locale}`
   );
 
   if (!response.ok) {
@@ -55,13 +43,7 @@ export async function fetchMentor(
   locale: string = "pt-BR"
 ): Promise<Mentor> {
   const response = await fetch(
-    `${STRAPI_API_URL}/api/mentores/${id}?populate=*&locale=${locale}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${STRAPI_TOKEN}`,
-      },
-    }
+    `${STRAPI_API_URL}/api/mentores/${id}?populate=*&locale=${locale}`
   );
 
   if (!response.ok) {
@@ -77,13 +59,7 @@ export async function fetchReviews(
   locale: string = "pt-BR"
 ): Promise<Review[]> {
   const response = await fetch(
-    `${STRAPI_API_URL}/api/avaliacoes?filters[curso][id][$eq]=${courseId}&populate=*&locale=${locale}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${STRAPI_TOKEN}`,
-      },
-    }
+    `${STRAPI_API_URL}/api/avaliacoes?filters[curso][id][$eq]=${courseId}&populate=*&locale=${locale}`
   );
 
   if (!response.ok) {
