38af176624fa211149e70d14e9a3bcf3c6454d69
AllanBSantos <allanbarrossantos@hotmail.com>
2025-04-11 16:21:17 -0300

    use postgres in production

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,110 +1,31 @@
 import path from "path";
 
-type DatabaseClient = "mysql" | "postgres" | "sqlite";
+export default ({ env }) => {
+  const client = env("DATABASE_CLIENT", "sqlite");
 
-interface DatabaseConfig {
-  connection: {
-    client: DatabaseClient;
-    host?: string;
-    port?: number;
-    database?: string;
-    user?: string;
-    password?: string;
-    ssl?: {
-      key?: string;
-      cert?: string;
-      ca?: string;
-      capath?: string;
-      cipher?: string;
-      rejectUnauthorized?: boolean;
-    };
-    connectionString?: string;
-    schema?: string;
-    filename?: string;
-    acquireConnectionTimeout?: number;
-  };
-  pool?: {
-    min: number;
-    max: number;
-  };
-  useNullAsDefault?: boolean;
-}
-
-export default ({ env }: any) => {
-  const client = env("DATABASE_CLIENT", "sqlite") as DatabaseClient;
-
-  const connections: Record<DatabaseClient, DatabaseConfig> = {
-    mysql: {
-      connection: {
-        client: "mysql",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true,
-          ),
-        },
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-    postgres: {
-      connection: {
-        client: "postgres",
-        connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true,
-          ),
-        },
-        schema: env("DATABASE_SCHEMA", "public"),
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-    sqlite: {
-      connection: {
-        client: "sqlite",
-        filename: path.join(
-          __dirname,
-          "..",
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db"),
-        ),
-      },
-      useNullAsDefault: true,
-    },
-  };
+  const isPostgres = client === "postgres";
 
   return {
     connection: {
       client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      connection: isPostgres
+        ? {
+            host: env("DATABASE_HOST"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME"),
+            user: env("DATABASE_USERNAME"),
+            password: env("DATABASE_PASSWORD"),
+            ssl: env.bool("DATABASE_SSL", true),
+          }
+        : {
+            filename: path.join(
+              __dirname,
+              "..",
+              "..",
+              env("DATABASE_FILENAME", ".tmp/data.db"),
+            ),
+          },
+      useNullAsDefault: !isPostgres,
     },
   };
 };
