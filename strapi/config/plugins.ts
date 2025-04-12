2534caad2a59861f0af067aa7c72f21694ee5c43
AllanBSantos <allanbarrossantos@hotmail.com>
2025-04-12 14:22:27 -0300

    use cloudinary to store  strapi files

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,18 +1,31 @@
 import path from "path";
 
-export default () => ({
-  "users-permissions": {
-    config: {
-      jwtSecret: process.env.JWT_SECRET || "your-secret-key",
-    },
-  },
-  upload: {
-    config: {
-      provider: "local",
-      providerOptions: {
-        sizeLimit: 1000000,
-        uploadPath: path.resolve("/opt/render/project/src/public/uploads"),
+export default ({ env }) => {
+  const isProduction = env("NODE_ENV") === "production";
+
+  return {
+    "users-permissions": {
+      config: {
+        jwtSecret: env("JWT_SECRET", "your-secret-key"),
       },
     },
-  },
-});
+    upload: {
+      config: isProduction
+        ? {
+            provider: "cloudinary",
+            providerOptions: {
+              cloud_name: env("CLOUDINARY_NAME"),
+              api_key: env("CLOUDINARY_KEY"),
+              api_secret: env("CLOUDINARY_SECRET"),
+            },
+          }
+        : {
+            provider: "local",
+            providerOptions: {
+              sizeLimit: 1000000,
+              uploadPath: path.resolve(__dirname, "..", "..", "public/uploads"),
+            },
+          },
+    },
+  };
+};
