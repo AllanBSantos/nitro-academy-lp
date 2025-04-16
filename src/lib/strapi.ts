abfd549b776bc6f7055bad9a0394d99d58c0c8d6
AllanBSantos <allanbarrossantos@hotmail.com>
2025-04-16 16:21:59 -0300

    fix: set locale hardCoded

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -2,12 +2,10 @@ import { Course, Mentor, Review } from "@/types/strapi";
 
 const STRAPI_API_URL =
   process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337";
-export async function fetchCourses(
-  locale: string = "pt-BR"
-): Promise<Course[]> {
+export async function fetchCourses(): Promise<Course[]> {
   try {
     const response = await fetch(
-      `${STRAPI_API_URL}/api/cursos?fields[0]=id&fields[1]=titulo&fields[2]=descricao&fields[3]=nota&fields[4]=nivel&fields[5]=modelo&fields[6]=objetivo&fields[7]=pre_requisitos&fields[8]=projetos&fields[9]=tarefa_de_casa&fields[10]=preco&fields[11]=parcelas&fields[12]=destaques&fields[13]=slug&fields[14]=link_pagamento&fields[15]=moeda&populate[imagem][fields][0]=url&populate[mentor][populate][imagem][fields][0]=url&populate[videos][populate]=video&populate[tags][fields][0]=nome&populate[cronograma][fields][0]=data_fim&populate[cronograma][fields][1]=data_inicio&populate[cronograma][fields][2]=dia&populate[cronograma][fields][3]=horario&populate[cronograma][fields][4]=faixa_etaria&locale=${locale}&_=${Date.now()}`
+      `${STRAPI_API_URL}/api/cursos?fields[0]=id&fields[1]=titulo&fields[2]=descricao&fields[3]=nota&fields[4]=nivel&fields[5]=modelo&fields[6]=objetivo&fields[7]=pre_requisitos&fields[8]=projetos&fields[9]=tarefa_de_casa&fields[10]=preco&fields[11]=parcelas&fields[12]=destaques&fields[13]=slug&fields[14]=link_pagamento&fields[15]=moeda&populate[imagem][fields][0]=url&populate[mentor][populate][imagem][fields][0]=url&populate[videos][populate]=video&populate[tags][fields][0]=nome&populate[cronograma][fields][0]=data_fim&populate[cronograma][fields][1]=data_inicio&populate[cronograma][fields][2]=dia&populate[cronograma][fields][3]=horario&populate[cronograma][fields][4]=faixa_etaria&locale=pt-BR&_=${Date.now()}`
     );
 
     if (!response.ok) {
@@ -23,12 +21,9 @@ export async function fetchCourses(
   }
 }
 
-export async function fetchCourse(
-  id: string,
-  locale: string = "pt-BR"
-): Promise<Course> {
+export async function fetchCourse(id: string): Promise<Course> {
   const response = await fetch(
-    `${STRAPI_API_URL}/api/cursos/${id}?populate=*&locale=${locale}`
+    `${STRAPI_API_URL}/api/cursos/${id}?populate=*&locale=pt-BR`
   );
 
   if (!response.ok) {
@@ -39,12 +34,9 @@ export async function fetchCourse(
   return data;
 }
 
-export async function fetchMentor(
-  id: number,
-  locale: string = "pt-BR"
-): Promise<Mentor> {
+export async function fetchMentor(id: number): Promise<Mentor> {
   const response = await fetch(
-    `${STRAPI_API_URL}/api/mentores/${id}?populate=*&locale=${locale}`
+    `${STRAPI_API_URL}/api/mentores/${id}?populate=*&locale=pt-BR`
   );
 
   if (!response.ok) {
@@ -55,12 +47,9 @@ export async function fetchMentor(
   return data;
 }
 
-export async function fetchReviews(
-  courseId: string,
-  locale: string = "pt-BR"
-): Promise<Review[]> {
+export async function fetchReviews(courseId: string): Promise<Review[]> {
   const response = await fetch(
-    `${STRAPI_API_URL}/api/avaliacoes?filters[curso][id][$eq]=${courseId}&populate=*&locale=${locale}`
+    `${STRAPI_API_URL}/api/avaliacoes?filters[curso][id][$eq]=${courseId}&populate=*&locale=pt-BR`
   );
 
   if (!response.ok) {
