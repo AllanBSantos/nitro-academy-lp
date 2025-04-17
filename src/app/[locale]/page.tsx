68bb7fcf8e67ccdc79934b70f9dbcceb373f5f58
AllanBSantos <allanbarrossantos@hotmail.com>
2025-04-17 10:25:37 -0300

    fix: removed old sections

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,12 +6,8 @@ import Header from "@/components/Header";
 import TeenGuy from "@/components/image-cards/TeenGuy";
 import Introduction from "@/components/text-cards/Introduction";
 import MiniUniversity from "@/components/text-cards/MiniUniversity";
-import ChildGirl from "@/components/image-cards/ChildGirl";
-import HaveYouImagined from "@/components/text-cards/HaveYouImagined";
 import OurApproach from "@/components/text-cards/OurApproach";
 import WhyChooseNitro from "@/components/text-cards/WhyChooseNitro";
-import Perks from "@/components/text-cards/Perks";
-import HowItWorks from "@/components/text-cards/HowItWorks";
 import ChildGuy from "@/components/image-cards/ChildGuy";
 import OurGoal from "@/components/text-cards/OurGoal";
 import Parents from "@/components/image-cards/Parents";
@@ -26,15 +22,9 @@ export default function Home({ params }: { params: { locale: string } }) {
       <TeenGuy />
       <Introduction />
       <MiniUniversity />
-      <ChildGirl />
-      <HaveYouImagined />
       <OurApproach />
       <CarouselClient locale={params.locale} />
       <WhyChooseNitro />
-      <div className="bg-theme-orange">
-        <Perks />
-        <HowItWorks />
-      </div>
       <ChildGuy />
       <OurGoal />
       <Parents />
