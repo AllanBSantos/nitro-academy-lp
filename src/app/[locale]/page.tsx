a085d7e650a457fcb0bc5ece765592f5e969b857
AllanBSantos <allanbarrossantos@hotmail.com>
2025-04-29 08:35:53 -0300

    feat: test page for VturbVideo

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -14,7 +14,8 @@ import Reviews from "@/components/Reviews";
 import Faq from "@/components/Faq";
 import CarouselClient from "@/components/CarouselClient";
 import Perks from "../components/text-cards/Perks";
-import VturbVideo from "@/components/VturbVideo";
+/* import VturbVideo from "@/components/VturbVideo"; */
+import MiniUniversity from "@/components/text-cards/MiniUniversity";
 
 export default function Home({ params }: { params: { locale: string } }) {
   setRequestLocale(params.locale);
@@ -23,9 +24,10 @@ export default function Home({ params }: { params: { locale: string } }) {
       <Header />
       <TeenGuy locale={params.locale} />
       <Introduction locale={params.locale} />
-      <div className="bg-theme-orange">
+      <MiniUniversity locale={params.locale} />
+      {/* <div className="bg-theme-orange">
         <VturbVideo />
-      </div>
+      </div> */}
       <OurApproach />
       <CarouselClient locale={params.locale} />
       <WhyChooseNitro locale={params.locale} />
