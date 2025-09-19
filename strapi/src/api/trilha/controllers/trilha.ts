798e077dd053d0f4a87057f14b134504ff1e3a9d
AllanBSantos <allanbarrossantos@hotmail.com>
2025-09-19 18:15:52 -0300

    chore: new collection trilha

---
+++ strapi/src/api/trilha/controllers/trilha.ts
@@ -0,0 +1,7 @@
+/**
+ * trilha controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::trilha.trilha');
---
+++ strapi/src/api/trilha/routes/trilha.ts
@@ -0,0 +1,7 @@
+/**
+ * trilha router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::trilha.trilha');
