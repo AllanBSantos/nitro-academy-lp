3b6f0b51fd279b56ca4be187e2483284eab9dd0b
AllanBSantos <allanbarrossantos@hotmail.com>
2025-09-23 13:31:07 -0300

    chore: new course field

--- strapi/types/generated/contentTypes.d.ts
+++ strapi/types/generated/contentTypes.d.ts
@@ -661,6 +661,7 @@ export interface ApiCursoCurso extends Struct.CollectionTypeSchema {
     badge: Schema.Attribute.Enumeration<
       ['nenhum', 'poucos_dias', 'poucas_vagas', 'dias_faltantes']
     >;
+    campanha: Schema.Attribute.Relation<'oneToOne', 'api::campanha.campanha'>;
     competencias: Schema.Attribute.Text;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
